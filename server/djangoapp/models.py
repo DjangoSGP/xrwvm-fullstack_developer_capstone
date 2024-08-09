d6619eb1c4259dfe3a5c51f975726e734396f5ff
Santiago <santigomezp13@gmail.com>
2024-08-09 19:31:49 -0400

    corrections

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,10 +1,12 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class CarMake(models.Model):
+
+
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
@@ -13,6 +15,8 @@ class CarMake(models.Model):
         return self.name  # Return the name as the string representation
 
 class CarModel(models.Model):
+
+    
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
